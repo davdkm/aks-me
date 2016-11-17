dcd61427b37d20c97aef7cbc9de9e60011d5be3b
davdkm <davd.km@gmail.com>
2016-11-16 17:49:28 -0800

    Paginate questions index

--- app/models/Question.rb
+++ app/models/Question.rb
@@ -1,9 +1,10 @@
 class Question < ActiveRecord::Base
   belongs_to :author,
              :class_name => "User"
-             
+
   has_many :comments
   has_many :commeters,
            :through => :comments,
            :source => :commenter
+  self.per_page = 10
 end
